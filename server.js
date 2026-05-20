eb2453ce5364019f3b53803aa28f28c0e157d3f9
Shane <semperfi.mosher@gmail.com>
2026-05-20 01:04:52 -0400

    Fix redirects and duplicate CORS

--- server.js
+++ server.js
@@ -56,11 +56,6 @@ app.use(session({
 
 /* ---------------- MIDDLEWARE ---------------- */
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
-
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(express.static(path.join(__dirname, "public")));
